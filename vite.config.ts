fd1e142495138b427f21aeef2655bfbd2b36fe9e
reyam2150 <reyam2150@gmail.com>
2026-08-13 10:38:14 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,5 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
-    spa: {
-      enabled: true,
-    },
   },
 });
