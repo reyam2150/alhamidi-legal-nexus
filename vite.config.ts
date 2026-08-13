0361d71cb7ade9828549c540972121f1d19c1517
reyam2150 <reyam2150@gmail.com>
2026-08-13 10:47:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,5 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
-
-    prerender: {
-      enabled: true,
-      autoSubfolderIndex: true,
-      autoStaticPathsDiscovery: false,
-      crawlLinks: false,
-      failOnError: true,
-    },
   },
 });
